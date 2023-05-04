fcc1e5e29cb6ccfbc859867fc617b183af10a2fd
Ceteris90 <jonas.nyador@mathmods.eu>
2023-05-03 18:08:00 -0700

    regular_expressions

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+#A ruby script that The regular expression must match School
+
+input_string = ARGV[0]
+puts input_string.scan(/School/).join()
---
+++ 0x06-regular_expressions/1-repetition_token.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+#A ruby script that The regular expression must match School
+
+input_string = ARGV[0]
+puts input_string.scan(/hbt{2,5}n/).join()
